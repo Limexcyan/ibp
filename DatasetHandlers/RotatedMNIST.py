fea1b2923580513f564a83a26648fc1b658448e9
limexcyan <lime.pss@gmail.com>
2025-04-14 13:40:43 +0000

    RotatedMNISTlist class implementation

--- DatasetHandlers/RotatedMNIST.py
+++ DatasetHandlers/RotatedMNIST.py
@@ -3,33 +3,50 @@ Implementation of RotatedMNIST for continual learning tasks.
 """
 
 import torch
+import copy
+import numpy as np
 from torchvision import datasets, transforms
 from torchvision.transforms import functional as F
 from PIL import Image
 import random
+from hypnettorch.data.mnist_data import MNISTData
 
 
-class RotatedMNIST(torch.utils.data.Dataset):
-    def __init__(self, root, train=True, angle=0, download=True):
-        self.mnist = datasets.MNIST(
-            root=root,
-            train=train,
-            download=download,
-            transform=transforms.ToTensor()
+class RotatedMNISTlist():
+    def __init__(
+            self,
+            rotations,
+            data_path,
+            use_not_hot=True,
+            validation_size=0,
+            padding=0,
+            trgt_padding=None,
+            show_rot_change_msg=True
+        ):
+
+        self._data = RotatedMNIST(
+            data_path,
+            use_not_hot=use_not_hot,
+            validation_size=validation_size,
+            rotation=None,
+            padding=padding,
+            trgt_padding=trgt_padding
         )
-        self.angle = angle  # could be a single int or list for more flexible cases
 
-    def __getitem__(self, index):
-        img, label = self.mnist[index]
+        self._rotations = rotations
 
-        # Convert to PIL if not already
-        if isinstance(img, torch.Tensor):
-            img = transforms.ToPILImage()(img)
+        self._show_perm_change_msg = show_rot_change_msg
 
-        # Rotate image
-        rotated_img = F.rotate(img, self.angle)
-
-        return transforms.ToTensor()(rotated_img), label
+        self._batch_gens_train = [None] * len(permutations)
+        self._batch_gens_test = [None] * len(permutations)
+        self._batch_gens_val = [None] * len(permutations)
 
     def __len__(self):
-        return len(self.mnist)
+        return len(self._rotations)
+
+    def __getitem__(self, index):
+        """Not implemented."""
+        raise NotImplementedError('Not yet implemented!')
+
+
+
